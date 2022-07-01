66a40e3796aab48c42fc9112b22abb671c023456
Jack Penzer <jackpenzer@MacBook-Air-4.local>
2022-07-01 21:27:36 +0100

    Added myself to contributors (eek!) Added globals for uncaught variables Reduced errors/warnings to 0/0 - 1 note remains. Temporarily removed @examples that call large models, let's move these to @usage

--- tests/testthat/test-transformers.R
+++ tests/testthat/test-transformers.R
@@ -5,3 +5,8 @@ test_that("load model fails if not given correct model nae", {
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+tokenizer <- hf_load_tokenizer("distilbert-base-uncased")
+test_that("load tokenizer function loads correctly", {
+  expect_true("vocab" %in% names(tokenizer))
+})
