da80821e9eafd7df6068babc11c2588d83f9986f
Jack Penzer <jackpenzer@MacBook-Air-4.local>
2022-07-02 10:18:27 +0100

    Adding documentation for models_with_downloads data + tibble dependency

---
+++ R/models_with_downloads.R
@@ -0,0 +1,10 @@
+#' Dataset of model names, tasks & download info
+#'
+#' \describe{
+#' #' \item{model}{name of model}
+#' #' \item{downloads}{number of downloads}
+#' #' \item{task}{task model performsl}
+#' #' \item{sha}{model's secure hash algorithm}
+#' #' \item{private}{whether model is public or private}
+#' }
+"models_with_downloads"
