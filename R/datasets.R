2e7e5fb042ef96516865300c1b72da60eb419075
Jack Penzer <jack.penzer@sharecreative.com>
2022-09-19 12:46:09 +0100

    datasets bug fix
    Changed first arg when adding split logic from "emotion" to dataset.

--- R/datasets.R
+++ R/datasets.R
@@ -2,7 +2,7 @@
 #'
 #' Function has multiple uses - getting pre-made datasets for exploratory analysis, or to figure as means for evaluating your fine-tuned models.
 #'
-#' @param dataset The name of a Hugging Face dataset saved on the Hub. Use hf_list_models() to find a dataset.
+#' @param dataset String - the name of a Hugging Face dataset saved on the Hub. Use hf_list_models() to find a dataset.
 #' @param split can be either 'train', 'test', 'validation', or left NULL for all three.
 #' @param as_tibble defaults to FALSE. Set to TRUE to return a tibble.
 #' @param label_name options are 'int2str' or 'str2int'. If as_tibble == TRUE this argument creates a new column 'label_name' that converts the label from an integer to a string or from a string to an integer.
@@ -62,7 +62,7 @@ hf_load_dataset <- function(dataset,
 
     # Now add splits logic
   } else if (!is.null(split) & as_tibble == TRUE) {
-    hf_data <- tibble::tibble(reticulate::py$load_dataset("emotion", split = split)$to_pandas())
+    hf_data <- tibble::tibble(reticulate::py$load_dataset(dataset, split = split)$to_pandas())
 
     # Now add str2int and int2str logic for splits (this could be refactored to not duplicate code, but ok for now)
     if (is.null(label_name)) {
