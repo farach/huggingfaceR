2f63a041fba0915d707604393c7a0b9fdc3ab972
Alex Farach <farach@users.noreply.github.com>
2022-12-28 13:02:10 -0500

    Debugging to pass all check()

--- tests/testthat/test-transformers.R
+++ tests/testthat/test-transformers.R
@@ -6,13 +6,12 @@ test_that("load model fails if not given correct model nae", {
 tokenizer <- hf_load_tokenizer("distilbert-base-uncased")
 slow_tokenizer <- hf_load_tokenizer('distilbert-base-uncased', use_fast = FALSE)
 
-
 test_that("incorrect model not loaded", {
-  expect_false(stringr::str_detect(tokenizer, "GPT|gpt"))
+  expect_false(stringr::str_detect(as.character(tokenizer), "GPT|gpt"))
 })
 
 test_that("tokenizer loads correct model", {
-  expect_true(stringr::str_detect(tokenizer,"distilbert"))
+  expect_true(stringr::str_detect(as.character(tokenizer),"distilbert"))
 })
 
 test_that("load tokenizer function loads correctly", {
@@ -20,11 +19,11 @@ test_that("load tokenizer function loads correctly", {
 })
 
 test_that("tokenizer function properly passess ...",{
-  expect_false(stringr::str_detect(slow_tokenizer, "is_fast=True"))
+  expect_false(stringr::str_detect(as.character(slow_tokenizer), "is_fast=True"))
 })
 
 test_that("tokenizer loads fast if not told otherwise by ...",{
-  expect_true(stringr::str_detect(tokenizer, "is_fast=True"))
+  expect_true(stringr::str_detect(as.character(tokenizer), "is_fast=True"))
 })
 
 
@@ -33,14 +32,14 @@ qa_model <- hf_load_AutoModel_for_task(model_type = "AutoModelForQuestionAnsweri
                               model_id = "deepset/roberta-base-squad2")
 
 test_that("Question Answering Model is of correct type", {
-  expect_true(stringr::str_detect(qa_model$config, "QuestionAnswering"))
+  expect_true(stringr::str_detect(as.character(qa_model$config), "QuestionAnswering"))
 })
 
 sent_model <- hf_load_AutoModel_for_task(model_id = "cardiffnlp/twitter-roberta-base-sentiment-latest",
                   model_type = "AutoModelForSequenceClassification")
 
 test_that("Sentiment model is of Sequence Classification type", {
-  expect_true(stringr::str_detect(sent_model$config, "ForSequenceClass"))
+  expect_true(stringr::str_detect(as.character(sent_model$config), "ForSequenceClass"))
 })
 
 
@@ -57,17 +56,16 @@ test_that("Sentiment model has label2id and id2label", {
 pipe <- hf_load_pipeline(model_id = "distilbert-base-uncased")
 pipe_qa <- hf_load_pipeline(model_id = "distilbert-base-uncased", task = "question-answering")
 
-
 test_that("hf_load_pipeline loads tokenizer", {
-  expect_true(stringr::str_detect(pipe$tokenizer, "distilb"))
+  expect_true(stringr::str_detect(as.character(pipe$tokenizer), "distilb"))
 })
 
 test_that("hf_load_pipeline does not load incorrect model", {
-  expect_false(stringr::str_detect(pipe$model, "GPT|gpt"))
+  expect_false(stringr::str_detect(as.character(pipe$model), "GPT|gpt"))
 })
 
 test_that("hf_load_pipeline loads correct model", {
-  expect_true(stringr::str_detect(pipe$model, "distilb"))
+  expect_true(stringr::str_detect(as.character(pipe$model), "distilb"))
 })
 
 test_that("hf_load_pipeline task argument is functioning", {
